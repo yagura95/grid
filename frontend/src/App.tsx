cecaf5541f0ae838727c8016be460a19f4a3f1c8
Joao Barbosa <joaob@evoke.pt>
2024-12-28 19:02:39 +0000

    secret code algorithm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,47 @@
 import "./App.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 async function getGrid() {
   const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/grid`) 
   const result = await response.json()
 
   return JSON.parse(result)
+}
+
+function normalizeValue(count: number): number {
+  if(count < 10) return count
+
+  const n = count.toString()
+  const divisor = parseInt(n[0]) + 1
+
+  return count / divisor
+}
+
+function getSecretCode(grid: string[][]) {
+  if(grid[0][0] === "") return 0 
+
+  const seconds = new Date().getSeconds().toString().padStart(2, "0")
+
+  const i = parseInt(seconds[0])
+  const j = parseInt(seconds[1])
+  
+  const firstLetter: string = grid[i][j]
+  const secondLetter: string = grid[j][i]
+
+  let firstLetterCount = 0
+  let secondLetterCount = 0
+
+  grid.forEach((row: string[]) => {
+    row.forEach((letter: string)=> {
+      if(letter === firstLetter)  return firstLetterCount++
+      if(letter === secondLetter) return secondLetterCount++
+    })
+  })
+
+  firstLetterCount = normalizeValue(firstLetterCount) 
+  secondLetterCount = normalizeValue(secondLetterCount) 
+
+  return firstLetterCount * 10 + secondLetterCount
 }
 
 const emptyGrid: string[][] = new Array(10).fill(new Array(10).fill(""))
@@ -22,6 +58,11 @@ function App() {
       setGrid(await getGrid())
     }, 2000)
   }
+
+  useEffect(() => {
+    const code = getSecretCode(grid)
+    setCode(code)
+  }, [grid])
 
   function verifyChar() {}
 

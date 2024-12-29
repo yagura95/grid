2201c96a16b89f6657e50ee23cce8e87e7813709
Joao Barbosa <joaob@evoke.pt>
2024-12-29 18:09:42 +0000

    bias/weighting factor input

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,72 @@
 const express = require("express")
 const cors = require('cors')
 
+const PORT = 4000
+const GRID_COLUMNS = 10
+const GRID_ROWS = 10
+const BIAS_PERCENT = 0.2
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function generateRandomChar() {
+  const min = 97  // ascii lowercase a 
+  const max = 122 // ascii lowercase z
+
+  return String.fromCharCode(generateRandomNumber(min, max))
+}
+
+function generateEmptyGrid(numRows, numColumns) {
+  const grid = [] 
+
+  for(let i = 0; i < numRows; i++) {
+    grid.push(new Array(numColumns).fill("")) 
+  }
+
+  return grid
+}
+
+function insertGridBias(grid, bias) {
+  let biasCount = GRID_ROWS * GRID_COLUMNS * BIAS_PERCENT 
+
+  while(biasCount > -1) {
+    const row = generateRandomNumber(0, GRID_ROWS - 1) 
+    const col = generateRandomNumber(0, GRID_COLUMNS - 1) 
+
+    if(!grid[row][col]) {
+      grid[row][col] = bias
+      biasCount--
+    } 
+  }
+}
+
+function generateGrid(bias) {
+  const grid = generateEmptyGrid(GRID_ROWS, GRID_COLUMNS)
+
+  if(bias) {
+    insertGridBias(grid, bias)
+  }
+
+  for(let row = 0; row < GRID_ROWS; row++) {
+    for(let col = 0; col < GRID_COLUMNS; col++) {
+      if(grid[row][col]) continue 
+
+      let char = generateRandomChar()
+
+      while(char === bias) {
+        char = generateRandomChar()
+      }
+
+      grid[row][col] = char
+    }
+  }
+
+  return grid
+}
+
 const app = express()
+
 app.use(
     cors({
         origin: true,
@@ -9,35 +74,12 @@ app.use(
     })
 )
 
-// Constants
-const PORT = 4000
-const GRID_COLUMNS = 10
-const GRID_ROWS = 10
+app.use(express.json())
 
-function generateRandomChar() {
-  const max = 122 // ascii lowercase z
-  const min = 97  // ascii lowercase a 
+app.post("/grid", (req, res) => {
+  const bias = req.body.bias
 
-  return String.fromCharCode(Math.floor(Math.random() * (max - min + 1) + min))
-}
-
-function generateGrid(rows, columns) {
-  const grid = [] 
-
-  for(let i = 0; i < rows; i++) {
-    const row = []
-    for(let j = 0; j < columns; j++) {
-      row.push(generateRandomChar()) 
-    }
-
-    grid.push(row)
-  }
-
-  return grid
-}
-
-app.get("/grid", (req, res) => {
-  const grid = generateGrid(GRID_ROWS, GRID_COLUMNS)
+  const grid = generateGrid(bias)
 
   res.json(JSON.stringify(grid))
 })
